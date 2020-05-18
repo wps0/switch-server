ffb6a41e0a01dce4f4ab80da4e159d3520f29636
wps0 <53907986+wps0@users.noreply.github.com>
2020-05-18 20:20:07 +0200

    Add SpotifyApiGateway auth retrieval api
    Signed-off-by: wps0 <53907986+wps0@users.noreply.github.com>

--- src/main/java/pl/wieczorkep/_switch/server/SwitchSound.java
+++ src/main/java/pl/wieczorkep/_switch/server/SwitchSound.java
@@ -2,6 +2,7 @@ package pl.wieczorkep._switch.server;
 
 import lombok.Getter;
 import pl.wieczorkep._switch.server.core.AppConfig;
+import pl.wieczorkep._switch.server.core.SpotifyApiGateway;
 import pl.wieczorkep._switch.server.core.concurrent.ConcurrencyManager;
 import pl.wieczorkep._switch.server.core.utils.ConfigUtils;
 import pl.wieczorkep._switch.server.view.ConsoleView;
@@ -13,6 +14,8 @@ public class SwitchSound {
     private static AppConfig config = new AppConfig(new ConsoleView());
     @Getter
     private static ConcurrencyManager concurrencyManager = new ConcurrencyManager(config);
+    @Getter
+    private static SpotifyApiGateway spotifyApiGateway;
 
     // ToDo: cmd arguments handling
     public static void main(String[] args) {
@@ -24,6 +27,10 @@ public class SwitchSound {
             config.getView().error(Arrays.toString(e.getStackTrace()));
         }
         concurrencyManager.init();
+
+        spotifyApiGateway = new SpotifyApiGateway("64f78c9a8a51413a86364dd9970dabb6",
+                "15c49b6ace0f421d94c73f59754e0018", "user-read-playback-state,user-modify-playback-state",
+                "http://localhost/");
     }
 
     public static AppConfig getConfig() {
--- src/main/java/pl/wieczorkep/_switch/server/core/SpotifyApiGateway.java
+++ src/main/java/pl/wieczorkep/_switch/server/core/SpotifyApiGateway.java
@@ -1,24 +1,34 @@
 package pl.wieczorkep._switch.server.core;
 
-import javax.management.RuntimeErrorException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
+import java.time.Instant;
 import java.util.Base64;
+
+import static java.lang.String.format;
 
 public class SpotifyApiGateway {
     public static final String API_ENDPOINT = "https://accounts.spotify.com";
     public static final String API_ENDPOINT_AUTH = API_ENDPOINT + "/authorize";
     public static final String API_ENDPOINT_REFRESH = API_ENDPOINT + "/api/token";
 
-    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_2)
-            .build();
-
     private final String APP_CLIENT_ID;
     private final String APP_CLIENT_SECRET;
     private final String APP_SCOPES;
     private final String APP_CALLBACKURL;
+
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+
+    private String authToken;
+    private String refreshToken;
+    private int validity;
+    private long lastRefresh;
 
     public SpotifyApiGateway(String appClientId, String appClientSecret, String appScopes, String appCallbackUrl) {
         APP_CLIENT_ID = appClientId;
@@ -27,6 +37,13 @@ public class SpotifyApiGateway {
         APP_CALLBACKURL = appCallbackUrl;
     }
 
+    public boolean isTokenValid() {
+        return (lastRefresh + validity) > Instant.now().getEpochSecond();
+    }
+
+    /**
+     * @return Spotify API url used by user to gain auth code
+     */
     public String getAuthUrl() {
         return API_ENDPOINT_AUTH.concat("?client_id=").concat(APP_CLIENT_ID)
                 .concat("&response_type=code")
@@ -34,10 +51,25 @@ public class SpotifyApiGateway {
                 .concat("&scope=").concat(APP_SCOPES);
     }
 
+    /**
+     * Exchanges access code to auth token.
+     * @param accessCode User access code received from the request to {@link #getAuthUrl()}
+     */
     public void exchangeAccessCodeToAuthToken(final String accessCode) {
+        makeSpotifyApiRequest("authorization_code",  "&code=" + accessCode + "&redirect_uri=http://localhost/");
+    }
+
+    public void refreshToken() {
+        if (refreshToken == null) {
+            throw new IllegalStateException("Refresh token was not set. Have you exchanged access token to auth token?");
+        }
+        makeSpotifyApiRequest("refresh_token", "&refresh_token=" + refreshToken);
+    }
+
+    private void makeSpotifyApiRequest(final String grantType, final String additionalArguments) {
         // POST to the API_ENDPOINT_REFRESH (probably https://accounts.spotify.com/api/token)
-        final String refreshRequestParams = "grant_type=authorization_code&code=" + accessCode +
-                "&redirect_uri=" + APP_CALLBACKURL;
+        final String refreshRequestParams = "grant_type=" + grantType + additionalArguments;
+        // HTTP refresh request to the spotify api
         HttpRequest refreshRequest = HttpRequest.newBuilder(URI.create(API_ENDPOINT_REFRESH))
                 .version(HttpClient.Version.HTTP_2)
                 .setHeader("Content-Type", "application/x-www-form-urlencoded")
@@ -46,29 +78,31 @@ public class SpotifyApiGateway {
                 .POST(HttpRequest.BodyPublishers.ofString(refreshRequestParams))
                 .build();
 
+        // Send request
         HttpResponse<String> refreshResponse = null;
         try {
             refreshResponse = HTTP_CLIENT.send(refreshRequest, HttpResponse.BodyHandlers.ofString());
         } catch (IOException | InterruptedException e) {
-            AppConfig.getLogger().warn("Failed to make Spotify API refresh request", e);
+            AppConfig.getLogger().error("Failed to make Spotify API refresh request", e);
         }
         AppConfig.getLogger().info("Spotify API request returned code " + refreshResponse.statusCode());
 
         if (refreshResponse.statusCode() != 200) {
-            AppConfig.getLogger().warn("Failed to make Spotify API request");
-            throw new RuntimeErrorException(new Error("HTTP response code is other than 200! Response from Spotify API: " + refreshResponse.body()));
+            AppConfig.getLogger().error("Failed to make Spotify API request");
+            throw new RuntimeException("HTTP response code (" + refreshResponse.statusCode() + ") is other than 200! Response from Spotify API: " + refreshResponse.body());
         }
-    }
 
-    public void refreshToken() {
+        // Parse response
+        JsonObject responseJson = JsonParser.parseString(refreshResponse.body()).getAsJsonObject();
+        AppConfig.getLogger().trace(format("Received JSON: %s", responseJson));
 
-    }
+        this.authToken = responseJson.get("access_token").getAsString();
+        if (grantType.equalsIgnoreCase("authorization_code")) {
+            this.refreshToken = responseJson.get("refresh_token").getAsString();
+        }
+        this.validity = responseJson.get("expires_in").getAsInt();
+        this.lastRefresh = Instant.now().getEpochSecond();
 
-    public static void main(String[] args) {
-        SpotifyApiGateway apiGateway = new SpotifyApiGateway("64f78c9a8a51413a86364dd9970dabb6",
-                "15c49b6ace0f421d94c73f59754e0018", "user-read-playback-state,user-modify-playback-state",
-                "http://localhost/");
-        System.out.println(apiGateway.getAuthUrl());
-        apiGateway.exchangeAccessCodeToAuthToken("");
+        AppConfig.getLogger().info(format("Access code to auth token successfully exchanged! Validity: %d.", validity));
     }
 }
