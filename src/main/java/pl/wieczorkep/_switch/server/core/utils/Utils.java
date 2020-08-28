cc6820a7203fd6a0c05ebdd846c28ee786a5ba9b
wps0 <53907986+wps0@users.noreply.github.com>
2020-08-28 23:44:39 +0200

    wip on query params stripping
    Signed-off-by: wps0 <53907986+wps0@users.noreply.github.com>

--- src/main/java/pl/wieczorkep/_switch/server/core/utils/Utils.java
+++ src/main/java/pl/wieczorkep/_switch/server/core/utils/Utils.java
@@ -2,6 +2,8 @@ package pl.wieczorkep._switch.server.core.utils;
 
 import lombok.extern.log4j.Log4j2;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
@@ -25,6 +27,32 @@ public class Utils {
             builder.append(AVAILABLE_CHARS.charAt(sr.nextInt(AVAILABLE_CHARS.length())));
             len--;
         }
+        return builder.toString();
+    }
+
+    public static String stripQuery(String query) {
+        query = URLEncoder.encode(query, StandardCharsets.US_ASCII);
+        int i = 0;
+        StringBuilder builder = new StringBuilder();
+
+        while (i < query.length()) {
+            if (query.charAt(i) != '%') {
+                builder.append(query.charAt(i));
+                i++;
+                continue;
+            }
+
+            int code = Integer.parseInt(query, i + 1, i + 3, 16);
+            if (code == '-') {
+                i += 3;
+                builder.append("%2D");
+                continue;
+            }
+            if (code < '0' || code > 'z' || code > '9' && code < 'A' || code > 'Z' && code < 'a') {
+                i += 3;
+            }
+        }
+
         return builder.toString();
     }
 }
